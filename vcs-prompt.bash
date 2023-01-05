0429a39f51977277d6ecce923b77f989e5738708
Ian Fitchet <ian.fitchet@runscripts.com>
2023-01-05 15:29:43 +0000

    vcs-prompt.bash
    Signed-off-by: Ian Fitchet <ian.fitchet@runscripts.com>

---
+++ vcs-prompt.bash
@@ -0,0 +1,51 @@
+
+if flag_set_p "i" ; then
+    vcss=()
+
+    if type -p git > /dev/null ; then
+	git_version=( $(git --version) )
+	for git_completion in /usr/share/doc/git{,-${git_version[2]},-core}/contrib/completion ; do
+	    git_bash_completion=${git_completion}/git-completion.bash
+	    if [[ -f ${git_bash_completion} ]] ; then
+		. ${git_bash_completion}
+		break
+	    fi
+	done
+	git_prompt=${git_completion}/git-prompt.sh
+	if [[ -f ${git_prompt} ]] ; then
+	    . ${git_prompt}
+	    export GIT_PS1_SHOWDIRTYSTATE=1
+	    export GIT_PS1_SHOWUPSTREAM="auto"
+	    vcss+=(git)
+	else
+	    __git_ps1 () { : ; }
+	fi
+    fi
+
+    if type -p hg > /dev/null ; then
+	vcss+=(hg)
+    fi
+
+    feature_description="${vcss[*]} prompts"
+
+    VCS () {
+	if [[ -d .hg || -d ../.hg || -d ../../.hg ]] ; then
+	    hg_rev=$(hg tip --template '{rev}/{branch} {tags}' 2>/dev/null)
+	    hg_bookmarks=$(hg bookmarks 2>/dev/null | awk '/\*/ {print $2}')
+	    echo "${hg_rev+ $(tty_effect green ${hg_rev})}${hg_bookmarks+ $(tty_effect magenta ${hg_bookmarks})}"
+	else
+	    #git_ps1=$(__git_ps1 "%s")
+	    #echo "${git_ps1+ $(tty_effect green ${git_ps1})}"
+	    __git_ps1 " ${tty_effect_green}%s${tty_effect_offattr}"
+	fi
+    }
+else
+    feature_description="n/a"
+fi
+
+provide vcs-prompt "${feature_description}"
+unset vcss feature_description
+
+# Local Variables:
+# mode: Shell-script
+# End:
