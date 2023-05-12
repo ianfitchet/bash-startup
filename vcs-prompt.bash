17199378ef29ad67c25d44bd27965c7123096fbc
Ian Fitchet <ian.fitchet@runscripts.com>
2023-05-12 12:31:02 +0100

    SHOWSTASHSTATE and SHOWCOLORHINTS

--- vcs-prompt.bash
+++ vcs-prompt.bash
@@ -38,7 +38,11 @@ if flag_set_p "i" ; then
 	    if [[ -f ${git_prompt} ]] ; then
 		. ${git_prompt}
 		export GIT_PS1_SHOWDIRTYSTATE=1
+		export GIT_PS1_SHOWSTASHSTATE=1
 		export GIT_PS1_SHOWUPSTREAM="auto"
+
+		export GIT_PS1_SHOWCOLORHINTS=1
+
 		vcss+=(git)
 		break
 	    else
